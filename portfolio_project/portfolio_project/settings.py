c6d53de860d2c1820b9e9930ad1a03df45e19c26
anyaga <anyaga@andrew.cmu.edu>
2025-01-12 16:19:04 -0500

    update settings.y

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ro6*gq(l2+=02u%ak7f=m15-y_$9hrc)69g5ll96d@6=xhvs3#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*","anyaga.github.io/portfolio/"]
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
