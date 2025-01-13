f84b7e3f18cdb2f3cf46565b17e804a840d1d09d
anyaga <anyaga@andrew.cmu.edu>
2025-01-12 19:31:46 -0500

    switch database of the whole project

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dq_portfolio',
+        'USER': 'anyaga',
+        'PASSWORD': 'Wanjiru137*',
+        'HOST': 'localhost', # Or use your host (e.g., '127.0.0.1')
+        'PORT': '5432', # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
